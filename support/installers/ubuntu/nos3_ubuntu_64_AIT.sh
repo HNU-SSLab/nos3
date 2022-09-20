8760df17f6def83d84242b26df6acd31631f3543
hig4342 <hig4342@naver.com>
2022-09-20 16:58:17 +0900

    Update AIT code

--- support/installers/ubuntu/nos3_ubuntu_64_AIT.sh
+++ support/installers/ubuntu/nos3_ubuntu_64_AIT.sh
@@ -15,11 +15,7 @@ echo "--- "
 echo 'nos3 ALL=(ALL) NOPASSWD: ALL' | sudo tee -a /etc/sudoers
 
 # Initialize variables
-if [[ -d /vagrant ]]; then
-  NOS3_USER=nos3
-else
-  NOS3_USER=$(logname)
-fi
+export NOS3_USER=nos3
 
 echo "Dependencies..."
 apt-get -y install python-pip socat 1> /dev/null
@@ -29,11 +25,11 @@ echo "Create necessary directories..."
 test -e /home/$NOS3_USER/AIT || mkdir /home/$NOS3_USER/AIT
 
 echo "Setup Environment..."
-cp /vagrant_parent/support/installers/common/nos3_99-firstboot.sh /home/$NOS3_USER/99-firstboot.sh 
+cp /home1/ncloud/nos3/support/installers/common/nos3_99-firstboot.sh /home/$NOS3_USER/99-firstboot.sh 
 dos2unix -q /home/$NOS3_USER/99-firstboot.sh 
 chmod 777 /home/$NOS3_USER/99-firstboot.sh 
 chown $NOS3_USER:$NOS3_USER /home/nos3/99-firstboot.sh
-cp /vagrant_parent/support/installers/centos/rc.local /etc/rc.local
+cp /home1/ncloud/nos3/support/installers/centos/rc.local /etc/rc.local
 dos2unix -q /etc/rc.local
 echo "export NOS3_USER=$(whoami)" >> /home/$NOS3_USER/.bashrc
 echo "export WORKON_HOME=/home/$NOS3_USER/.virtualenvs" >> /home/$NOS3_USER/.bashrc
@@ -41,11 +37,11 @@ echo "export PROJECT_HOME=/home/$NOS3_USER/Devel" >> /home/$NOS3_USER/.bashrc
 echo "source /usr/local/bin/virtualenvwrapper.sh" >> /home/$NOS3_USER/.bashrc
 	
 echo "Setup rc.local..."
-cp /vagrant_parent/support/installers/ubuntu/rc.local /etc/rc.local
+cp /home1/ncloud/nos3/support/installers/ubuntu/rc.local /etc/rc.local
 dos2unix -q /etc/rc.local
 
 echo "Setup Postactivate..."
-cp /vagrant_parent/support/installers/ubuntu/postactivate /home/$NOS3_USER/
+cp /home1/ncloud/nos3/support/installers/ubuntu/postactivate /home/$NOS3_USER/
 dos2unix -q /home/$NOS3_USER/
 chown $NOS3_USER:$NOS3_USER /home/$NOS3_USER/postactivate
 
